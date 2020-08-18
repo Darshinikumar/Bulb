deefc1a030d8c8b5db0142454ff796d6d8a8b4c1
Darshini <55615942+Darshinikumar@users.noreply.github.com>
2020-08-18 11:19:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+function lightBulb() {
+  var image = document.getElementById('myBulb');
+  if (image.src.match("on")) {
+    image.src = "bulbturned_off.png";
+  } else {
+    image.src = "bulbturned_on.png";
+  }
+}
+
+function lightBulb2() {
+  var image = document.getElementById('myBulb2');
+  if (image.src.match("on")) {
+    image.src = "bulbturned_off.png";
+  } else {
+    image.src = "bulbturned_on.png";
+  }
+}
+
+
+function lightBulb3() {
+  var image = document.getElementById('myBulb3');
+  if (image.src.match("on")) {
+    image.src = "bulbturned_off.png";
+  } else {
+    image.src = "bulbturned_on.png";
+  }
+}
+
+
+function lightBulb4() {
+  var image = document.getElementById('myBulb4');
+  if (image.src.match("on")) {
+    image.src = "bulbturned_off.png";
+  } else {
+    image.src = "bulbturned_on.png";
+  }
+}
+
+
+
